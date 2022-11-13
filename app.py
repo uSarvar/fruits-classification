a89edad3c49d50f832de8a60fb8b96d849d3f5b5
Sarvar Anvarovich <61342503+uSarvar@users.noreply.github.com>
2022-11-13 14:39:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ if plt == 'Linux' : pathlib.WindowsPath = pathlib.PosixPath
 # title
 st.title('Classification of fruits')
 st.subheader('This app predicts which of the classes (strawberry, lemon, grape, banana and apple)\
- an image uploaded to the app belongs to. You can upload an image or take a pic from your device.')
+ an image uploaded to the app belongs to. You can upload an image from your device.')
 
 # upload pics
 file = st.file_uploader('Upload image', type=['png','jpeg','jpg','gif','svg'])
-pic = st.camera_input('Take a picture')
 
 # model
 model = load_learner('fruit_model1.pkl')
